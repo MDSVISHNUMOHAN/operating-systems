421114b0ae46a732a4d889486c13ac1cb9d65f04
MDSVISHNUMOHAN <134498482+MDSVISHNUMOHAN@users.noreply.github.com>
2023-05-24 15:48:40 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,13 @@
+# #creating a simple python function to add two numbers
+# def add(int):
+#     x=eval(input("enter a number: "))
+#     y=eval(input("enter the second number: "))
+#     # print(x+y)
+# add(int)
+
+def add(a,b):
+    return(a+b)
+a=float(input("enter a value: "))
+b=float(input("enter the second value: "))
+print(a+b)
+ add(a,b)
